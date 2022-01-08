d43df0f1a6b94ea0101757958aa0861b09e39a2e
Andreea Ionita <ionitaandreea2608@gmail.com>
2022-01-08 02:49:55 +0200

    Varianta care merge perfect pana la pasul 7

---
+++ AgendaProf/AgendaProf/Models/ListStudent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+namespace AgendaProf.Models
+{
+    public class ListStudent
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        [ForeignKey(typeof(NoteList))]
+        public int NoteListID { get; set; }
+        public int StudentID { get; set; }
+
+    }
+}
---
+++ AgendaProf/AgendaProf/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+
+namespace AgendaProf.Models
+{
+    public class Student
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Description { get; set; }
+        [OneToMany]
+        public List<ListStudent> ListStudents { get; set; }
+    }
+}
